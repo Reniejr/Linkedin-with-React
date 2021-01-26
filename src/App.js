9b0991dbc6bc1b8271a84c5b6c6e5ce7c695240a
Hung Jin Chong <hungjinchong@Outlook.com>
2021-01-26 11:12:30 +0000

    added authentication

--- src/App.js
+++ src/App.js
@@ -14,89 +14,87 @@ import Home from "./components/main_components/Home";
 import NetworkPage from "./components/Network_Components/NetworkPage";
 import JobsPage from "./components/Jobs_Compnents/JobsPage";
 import MsgPage from "./components/Messaging_Component/MsgPage";
+import ProtectedRoute from "./components/authComponents/ProtectedRoute";
+import NotRegistered from "./components/authComponents/NotRegistered";
+
 class App extends React.Component {
-	state = {
-		userList: [],
-		filteredUserList: [],
-		showResult: false,
-		searchString: "",
-	};
+  state = {
+    userList: [],
+    filteredUserList: [],
+    showResult: false,
+    searchString: "",
+  };
 
-	getUserList = async () => {
-		try {
-			const response = await fetch(
-				process.env.REACT_APP_BASE_URL + "profile/",
-				{
-					headers: {
-						Authorization: `Bearer ${process.env.REACT_APP_ACCESS_TOKEN}`,
-					},
-				}
-			);
-			const userList = await response.json();
-			console.log("All user list", userList);
-			this.setState({ userList });
-		} catch (err) {
-			console.log(err);
-		}
-	};
+  getUserList = async () => {
+    try {
+      const response = await fetch(
+        process.env.REACT_APP_BASE_URL + "profile/",
+        {
+          headers: {
+            Authorization: `Bearer ${process.env.REACT_APP_ACCESS_TOKEN}`,
+          },
+        }
+      );
+      const userList = await response.json();
+      console.log("All user list", userList);
+      this.setState({ userList });
+    } catch (err) {
+      console.log(err);
+    }
+  };
 
-	handleSearch = (e) => {
-		if (e.keyCode === 13 || e.key === "Enter") {
-			e.preventDefault();
+  handleSearch = (e) => {
+    if (e.keyCode === 13 || e.key === "Enter") {
+      e.preventDefault();
 
-			let filteredUserList = this.state.userList.filter(
-				(user) =>
-					(user.name &&
-						user.name
-							.toLowerCase()
-							.includes(this.state.searchString)) ||
-					(user.surname &&
-						user.surname
-							.toLowerCase()
-							.includes(this.state.searchString))
-			);
+      let filteredUserList = this.state.userList.filter(
+        (user) =>
+          (user.name &&
+            user.name.toLowerCase().includes(this.state.searchString)) ||
+          (user.surname &&
+            user.surname.toLowerCase().includes(this.state.searchString))
+      );
 
-			this.setState({ filteredUserList });
-			this.setState({ showResult: true });
-		} else {
-			this.setState({
-				searchString: e.currentTarget.value,
-				filteredUserList: this.state.userList,
-			});
-		}
-	};
+      this.setState({ filteredUserList });
+      this.setState({ showResult: true });
+    } else {
+      this.setState({
+        searchString: e.currentTarget.value,
+        filteredUserList: this.state.userList,
+      });
+    }
+  };
 
-	componentDidMount() {
-		this.getUserList();
-	}
-	render() {
-		return (
-			<div className='App'>
-				<Router>
-					<ShowSearchResult
-						keyword={this.state.searchString}
-						users={this.state.filteredUserList}
-						onHide={() => this.setState({ showResult: false })}
-						show={this.state.showResult}
-					/>
-					<NavBar
-						searchString={this.state.searchString}
-						handleSearch={this.handleSearch}
-					/>
+  componentDidMount() {
+    this.getUserList();
+  }
+  render() {
+    return (
+      <div className="App">
+        <Router>
+          <ShowSearchResult
+            keyword={this.state.searchString}
+            users={this.state.filteredUserList}
+            onHide={() => this.setState({ showResult: false })}
+            show={this.state.showResult}
+          />
+          <NavBar
+            searchString={this.state.searchString}
+            handleSearch={this.handleSearch}
+          />
 
-					<Route path='/' exact component={Home} />
-					<Route path='/profile/:id' component={Profile} />
-					<Route
-						path='/mynetwork'
-						render={(props) => (
-							<NetworkPage userList={this.state.userList} />
-						)}
-					/>
-					<Route path='/jobs' component={JobsPage} />
-					<Route path='/message' component={MsgPage} />
-				</Router>
-			</div>
-		);
-	}
+          <ProtectedRoute path="/" exact component={Home} />
+          <ProtectedRoute path="/profile/:id" component={Profile} />
+          <Route
+            path="/mynetwork"
+            render={(props) => <NetworkPage userList={this.state.userList} />}
+          />
+          <Route path="/jobs" component={JobsPage} />
+          <Route path="/message" component={MsgPage} />
+          <Route path="/logout" component={NotRegistered} />
+        </Router>
+      </div>
+    );
+  }
 }
 export default App;
