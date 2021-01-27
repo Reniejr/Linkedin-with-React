adcafccbbde7f439cd6a4265c6638b59c98330d8
Reniejr <reniejr@gmail.com>
2021-01-27 14:43:08 +0100

    updates

--- src/components/Messaging_Component/MsgPage.jsx
+++ src/components/Messaging_Component/MsgPage.jsx
@@ -5,7 +5,7 @@ import "../css/RightSide.scss";
 import MsgSide from "./MsgSide";
 import footericon from "../images/footericon.png";
 import Pusher from "pusher-js";
-import { getUser } from "./Utils/index";
+import { getUser, getAllProfiles, updateChannel } from "./Utils/index";
 import axios from "axios";
 import { withAuth0 } from "@auth0/auth0-react";
 
@@ -14,42 +14,87 @@ class MsgPage extends PureComponent {
     text: "",
     username: "",
     chats: [],
+    allUsers: [],
+    chatSelected: "",
+  };
+
+  pusherSetup = (allUsers) => {
+    const pusher = new Pusher(process.env.REACT_APP_PUSHER_KEY, {
+      cluster: process.env.REACT_APP_PUSHER_CLUSTER,
+      encrypted: true,
+    });
+    let channels = [];
+    allUsers.map((user, index) => {
+      const channel = pusher.subscribe(`${index}`);
+      channels.push(channel);
+    });
+    return channels;
   };
 
   setUser = async () => {
     const { user } = this.props.auth0;
     let currentId = user.sub.slice(6);
-    // let id;
-    // this.props.match.params.id === "me"
-    //   ? (id = process.env.REACT_APP_ID)
-    //   : (id = process.env.REACT_APP_ID);
     const getUserInfo = await getUser(`${currentId}`);
     this.setState({ username: getUserInfo.username });
+    console.log(getUserInfo.username);
   };
 
-  componentDidMount() {
-    const { user } = this.props.auth0;
-    let currentId = user.sub.slice(6);
-    console.log(currentId);
-    this.setUser();
-    const pusher = new Pusher(process.env.REACT_APP_PUSHER_KEY, {
-      cluster: process.env.REACT_APP_PUSHER_CLUSTER,
-      encrypted: true,
+  setAllUsers = async () => {
+    const allUsers = await getAllProfiles(this.state.username);
+    this.setState({ allUsers });
+    let allChats = [];
+    allUsers.map((user, index) => {
+      let chatBox = {
+        chatId: index,
+        user: user.username,
+        chat: [],
+      };
+      allChats.push(chatBox);
     });
-    const channel = pusher.subscribe("chat");
+    this.setState({ chats: allChats });
+  };
+
+  setChat = (e) => {
+    let chatSelected = e.currentTarget.value;
+    this.setState({ chatSelected });
+
+    let index = this.state.chats.findIndex(
+      (user) => user.user === chatSelected
+    );
+
+    const pusher = this.pusherSetup(this.state.allUsers);
+    console.log(pusher);
+    const channel = pusher.filter(
+      (channel) => channel.name === index.toString()
+    )[0];
     channel.bind("message", (data) => {
-      this.setState({ chats: [...this.state.chats, data], test: "" });
+      // Original
+      this.setState({ chats: [...this.state.chats, data] });
     });
     this.typeText = this.typeText.bind(this);
-  }
+  };
 
-  typeText = (e) => {
+  componentDidMount = async () => {
+    await this.setUser();
+    await this.setAllUsers();
+  };
+
+  typeText = async (e) => {
     if (e.keyCode === 13) {
       const payload = {
-        username: this.state.username,
-        message: this.state.text,
+        message: {
+          username: this.state.username,
+          message: this.state.text,
+        },
+        chat: `${this.state.chatSelected}`,
       };
-      axios.post(`${process.env.REACT_APP_BASE_URL}/chat`, payload);
+      let array = updateChannel(
+        this.state.chats,
+        this.state.chatSelected,
+        payload.message
+      );
+      axios.post(`${process.env.REACT_APP_BASE_URL}/chat`, array);
+      e.currentTarget.value = " ";
     } else {
       let text = e.currentTarget.value;
       this.setState({ text: text });
@@ -60,11 +105,16 @@ class MsgPage extends PureComponent {
     return (
       <div id="msg-page">
         <div className="main-body">
-          <MsgSide />
+          <MsgSide allUsers={this.state.allUsers} setChat={this.setChat} />
           <MainMsg
             typeFunc={this.typeText}
             chat={this.state.chats}
             currentUser={this.state.username}
+            currentChat={
+              this.state.chats.filter(
+                (chat) => chat.user === this.state.chatSelected
+              )[0]
+            }
           />
         </div>
         <div id="footer-right" style={{ position: "sticky", top: "60px" }}>
--- src/components/Messaging_Component/MsgSide.jsx
+++ src/components/Messaging_Component/MsgSide.jsx
@@ -2,7 +2,12 @@ import React, { PureComponent } from "react";
 import "./Messaging_Styles/MsgSide.scss";
 
 export default class MsgSide extends PureComponent {
+  setChat = (e) => {
+    this.props.setChat(e);
+  };
+
   render() {
+    let { allUsers, setChat } = this.props;
     return (
       <div id="msg-side">
         <header>
@@ -15,11 +20,38 @@ export default class MsgSide extends PureComponent {
         <div className="msg-side-body">
           <input type="text" placeholder="Search Messages" />
           <div className="msg-container">
-            <p>No messages...yet!</p>
-            <p>
-              Reach out and start a conversation. <br /> Great things might
-              happen
-            </p>
+            {allUsers.length === 0 ? (
+              <div>
+                <p>No messages...yet!</p>
+                <p>
+                  Reach out and start a conversation. <br /> Great things might
+                  happen
+                </p>
+              </div>
+            ) : (
+              <ul className="listUsers">
+                {allUsers.map((user) => {
+                  const randomColor = Math.floor(
+                    Math.random() * 16777215
+                  ).toString(16);
+                  return (
+                    <li
+                      key={user._id}
+                      style={{
+                        borderColor: `#${randomColor}`,
+                        color: `#${randomColor}`,
+                      }}
+                    >
+                      <input
+                        type="button"
+                        value={user.username}
+                        onClick={(e) => this.setChat(e)}
+                      />
+                    </li>
+                  );
+                })}
+              </ul>
+            )}
           </div>
           <button>Start a new message</button>
         </div>
