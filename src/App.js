77227ede0b412f0eacbad3c62559be58799d45a9
Reniejr <reniejr@gmail.com>
2021-01-28 15:23:41 +0100

    updates

--- src/App.js
+++ src/App.js
@@ -5,7 +5,8 @@ import NavBar from "./components/NavBar";
 
 import Profile from "./components/main_components/Profile";
 
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { Route } from "react-router-dom";
+import {HashRouter as Router} from "react-router-dom"
 
 import ShowSearchResult from "./components/main_components/ShowSearchResult";
 
